5afe2e29172b0446a84d613d5698e807ef8dd332
Mateusz Rosiński <matirosinski02@gmail.com>
2022-04-18 21:14:58 +0200

    Get game data

---
+++ Data/dataQuotes.js
@@ -0,0 +1,17 @@
+export const dataQuotes = [
+  {
+    id: 0,
+    category: 'Literatura',
+    word: 'pan tadeusz'
+  },
+  {
+    id: 1,
+    category: 'Supersamochody',
+    word: 'bugatti chiron'
+  },
+  {
+    id: 2,
+    category: 'Gra komputerowa',
+    word: 'Genshin Impact'
+  }
+]
